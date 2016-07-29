78ee2d02c52cb2f5885cd049f6a5690d79e0e046
Franklin Wesley <franklin.wesley.bastos@gmail.com>
2016-07-29 11:00:31 -0300

    routes and model problem

---
+++ src/main/java/com/ufcg/controllers/ProblemController.java
@@ -0,0 +1,38 @@
+package com.ufcg.controllers;
+
+import com.ufcg.models.Problem;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by franklin on 29/07/16.
+ */
+@RestController
+@RequestMapping(value="/problem")
+public class ProblemController {
+
+    @RequestMapping(value="", method= RequestMethod.GET)
+    public String getProblems(@RequestParam(value = "page", defaultValue = "1") int page,
+                              @RequestParam(value = "sort", defaultValue = "date") String sort){
+        return "Get Problems";
+    }
+
+    @RequestMapping(value="/{id}", method= RequestMethod.GET)
+    public String getProblem(@PathVariable("id") String problemId){
+        return "Get Problem " + problemId;
+    }
+
+    @RequestMapping(value="", method= RequestMethod.POST)
+    public String createProblem(@RequestBody Problem problem){
+        return "Create Problem";
+    }
+
+    @RequestMapping(value="/{id}", method= RequestMethod.PUT)
+    public String updateProblem(@PathVariable("id") String problemId, @RequestBody Problem problem){
+        return "Update Problem " + problemId + "\n" + problem.toString();
+    }
+
+    @RequestMapping(value="/{id}", method= RequestMethod.DELETE)
+    public String deleteProblem(@PathVariable("id") String problemId){
+        return "Delete Problem " + problemId;
+    }
+}
---
+++ src/main/java/com/ufcg/models/Problem.java
@@ -0,0 +1,55 @@
+package com.ufcg.models;
+
+import java.io.Serializable;
+
+/**
+ * Created by huawei on 29/07/16.
+ */
+public class Problem implements Serializable{
+
+    private String name;
+    private String description;
+    private Long id;
+
+    public Problem() {
+    }
+
+    public Problem(String name, String description, Long id) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Problem{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
