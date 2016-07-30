89fe9c3ec27aafe5471f7dcd15bb5622f3792853
Franklin Wesley <franklin.wesley.bastos@gmail.com>
2016-07-30 13:24:47 -0300

    update Swagger Documentation

--- src/main/java/com/ufcg/DacaApplication.java
+++ src/main/java/com/ufcg/DacaApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static com.google.common.base.Predicates.not;
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -25,8 +23,8 @@ public class DacaApplication {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())
 				.select()
-//				.paths(PathSelectors.regex("/basic-error-controller.*"))
-                .paths(not(regex("/error/.*")))
+				.paths(PathSelectors.regex("/basic-error-controller.*"))
+//                .paths(Predicates.not(PathSelectors.regex('/error.*')))
 				.build();
 	}
 
